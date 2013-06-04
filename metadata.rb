acf2870f3d48c00efdd3605179a46f8b3a9c4368
Andreas Lappe <nd@off-pist.de>
2013-06-04 09:41:54 +0200

    Bump version to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nd@kaeufli.ch'
 license          'MIT'
 description      'Installs/Configures opensmtpd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 recipe "opensmtpd", "Installs OpenSMTPD from source"
 supports "ubuntu"
